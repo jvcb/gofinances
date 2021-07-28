46ebbad0bf6f40f3391fb7e3a4dbb6f01bb2040f
jvcb <jvcb.dev@gmail.com>
2021-07-27 21:52:26 -0300

    completed listing

--- src/components/HighlightCard/index.tsx
+++ src/components/HighlightCard/index.tsx
@@ -10,17 +10,44 @@ import {
   LastTransaction
 } from './styles';
 
-export function HighlightCard() {
+interface Props {
+  type: 'up' | 'down' | 'total';
+  title: string;
+  amount: string;
+  lastTransation: string;
+}
+
+const icon = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle',
+  total: 'dollar-sign'
+}
+
+export function HighlightCard({
+  type, 
+  title, 
+  amount, 
+  lastTransation
+}: Props) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entrada</Title>
-        <Icon name="arrow-up-circle"></Icon>
+        <Title type={type}>
+          { title }
+        </Title>
+        <Icon 
+          name={icon[type]} 
+          type={type}
+        ></Icon>
       </Header>
 
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransaction>Última entrada dia 21 de julho</LastTransaction>
+        <Amount type={type}>
+          { amount }
+        </Amount>
+        <LastTransaction type={type}>
+          { lastTransation }
+        </LastTransaction>
       </Footer>
     </Container>
   );
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container, 
@@ -12,10 +13,53 @@ import {
   UserGreeting,
   UserName,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title: "Salário do mês",
+      amount: "R$ 4.750,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      },
+      date: "13/04/2020",
+    },
+    {      
+      id: '2',
+      type: 'negative',
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee'
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title: "Aluguel do apartamento",
+      amount: "R$ 600,00",
+      category: {
+        name: 'Alimentação',
+        icon: 'shopping-bag'
+      },
+      date: "13/04/2020",
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -34,10 +78,37 @@ export function Dashboard() {
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          type="up"
+          title="Entradas" 
+          amount="R$ 4.750,00" 
+          lastTransation="Última entrada dia 13 de abril" 
+        />
+        <HighlightCard
+          type="down" 
+          title="Saídas" 
+          amount="R$ 659,00" 
+          lastTransation="Última entrada dia 03 de abril" 
+        />
+        <HighlightCard
+          type="total" 
+          title="Total" 
+          amount="R$ 4.091,00" 
+          lastTransation="01 à 16 de abril" 
+        />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList 
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
+
+
     </Container>
   );
 }
